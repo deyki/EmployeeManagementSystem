5ec60e14b8a0972af67d1017b1bc366ff408ad23
deyki <deikioveca@gmail.com>
2022-11-24 20:45:05 +0200

    ResponseMessage

---
+++ src/main/java/EMS/EmployeeManagementSystem/models/message/ResponseMessage.java
@@ -0,0 +1,11 @@
+package EMS.EmployeeManagementSystem.models.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseMessage {
+
+    private String responseMessage;
+}
