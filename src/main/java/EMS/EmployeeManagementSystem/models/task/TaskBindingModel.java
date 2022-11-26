c82d879858de58ceb59bfde3939cd6f3b5bfc829
deyki <deikioveca@gmail.com>
2022-11-26 11:08:22 +0200

    TaskBindingModel

---
+++ src/main/java/EMS/EmployeeManagementSystem/models/task/TaskBindingModel.java
@@ -0,0 +1,12 @@
+package EMS.EmployeeManagementSystem.models.task;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class TaskBindingModel {
+
+    private String username;
+    private String content;
+}
